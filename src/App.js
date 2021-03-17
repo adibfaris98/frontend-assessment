3628d0f70169ba677016d314d8505c9569dd7119
Adib <adibfaris98@gmail.com>
2021-03-17 13:54:37 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,39 +2,31 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
+  Flex
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { ColorModeSwitcher } from './component/ColorModeSwitcher';
+import Card from './component/Card'
+import CardLarge from './component/CardLarge'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Flex justifyContent="flex-end">
+        <ColorModeSwitcher />
+      </Flex>
+
+      <Flex justifyContent="center" alignItems="space-between">
+        <Box width="full" height="full" borderWidth="thick">
+          <CardLarge />
+        </Box>
+        <Box width="full" height="full" >
+          <Card />
+          <Card />
+        </Box>
+      </Flex>
+
     </ChakraProvider>
   );
 }
---
+++ src/component/Card.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Box, Image, Text, Link } from '@chakra-ui/react'
+
+export default function Card() {
+    return (
+        <Box mt={5}>
+            <Box p={4} display="flex" border="1px" borderRadius="10">
+                <Box flexShrink={0}>
+                    <Image
+                        borderRadius="lg"
+                        width={40}
+                        src="https://bit.ly/2jYM25F"
+                        alt="Woman paying for a purchase"
+                    />
+                </Box>
+                <Box mt={0} ml={6}>
+                    <Text
+                        fontWeight="bold"
+                        textTransform="uppercase"
+                        fontSize="lg"
+                        letterSpacing="wide"
+                        color="orange.600"
+                    >
+                        Temperature
+    </Text>
+                    <Link
+                        mt={1}
+                        display="block"
+                        fontSize="lg"
+                        lineHeight="normal"
+                        fontWeight="semibold"
+                        href="#"
+                    >
+                        Weather
+    </Link>
+                    <Text mt={2} color="gray.500">
+                        Date
+</Text>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
